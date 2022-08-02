e08ec2ac630f1a610e4b38fbc359403515b4eaa0
Edwin Melara <edwin.melara.landaverde@gmail.com>
2022-08-02 00:01:40 -0600

    Inserccion

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,11 +1,19 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Pokemon } from './entities/pokemon.entity';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
+  constructor(@InjectModel(Pokemon.name) private pokemon: Model<Pokemon>) {}
+
   create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+    createPokemonDto.name = createPokemonDto.name.toLowerCase();
+    const pokemonCreated = new this.pokemon(createPokemonDto);
+    return pokemonCreated.save();
   }
 
   findAll() {
