9623b9c362351008ffcb99bbab312a40084ce5ec
Edwin Melara <edwin.melara.landaverde@gmail.com>
2022-08-01 22:31:25 -0600

    Resource Pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    PokemonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
